bb6ec9c2cfceb2ee0ca3f140fc4840e4104f8ba6
Tony Wong <tdubb123@gmail.com>
2022-04-05 06:01:50 -0700

    corrected backend remote

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "TDUBB-Terraform-Demos"
+    organization = "tdubb-AWS-demo"
 
     workspaces {
       name = "demo-github-actions"
